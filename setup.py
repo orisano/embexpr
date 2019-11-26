0d1572d9c9f1c727432cea3c7bb3cb5c20189e06
Nao YONASHIRO <owan.orisano@gmail.com>
2019-11-27 00:04:00 +0900

    docs: add 3.8 support

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
